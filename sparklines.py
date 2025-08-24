d636d5205a49b094e44b0607b72206d295426fbf
HealthRecords <github.com3@worldware.com>
2025-08-23 21:38:56 -0700

    Updated sparklines
    Moved to table format.
    Make graph min and max separate from normal range min and max.

--- sparklines.py
+++ sparklines.py
@@ -1,5 +1,5 @@
 """
-This is an independent file, to test sparkline generation.
+This file generates a set of sparklines. Currrently hard coded.
 
 https://www.markhneedham.com/blog/2017/09/23/python-3-create-sparklines-using-matplotlib/
 
@@ -11,29 +11,28 @@ could also maybe use pygal, which sounds cool. But let's use matplotlib for now.
 
 TODO: Need to match the date range across all sparklines, if I'm going to line them up.
 
-TODO: My current plan is to make this the main program for generating sparklines, and have it call health.py functions
-      to acquire the data to plot.
-      First step, make this work on list[Observation]. Observation may need to me modified to include normal ranges.
-TODO: I need normal range for every possible test. It looks like what I want is in Observation-*.json files,
-      as referenceRange. It's in 2707 out of 4541 files. Next step is to pass in a hard coded range, and plot
-      those, then I can worry about getting the actual numbers.
+TODO: I need normal range for every test we want to plot (I guess it's not required, just nice to have).
+      It looks like what I want is in Observation-*.json files,
+      as referenceRange. It's in 2707 out of 4541 files.
 """
 import base64
 from datetime import datetime
 from io import BytesIO
 from pathlib import Path
 from typing import TextIO
-from matplotlib import pyplot as plt
 
-from health import extract_all_values, yield_observation_files, Observation
+from matplotlib import pyplot as plt
 import matplotlib.dates as mdates
 
-def sparkline(data_x_str: list[str], data_y: list[float], min_normal, max_normal):
+from health import extract_all_values, yield_observation_files, Observation
+
+def sparkline(data_x_str: list[str], data_y: list[float], graph_y_min, graph_y_max, normal_min, normal_max):
     data_x = [datetime.strptime(date, '%Y-%m-%dT%H:%M:%SZ') for date in data_x_str]
 
     fig, axes = plt.subplots(1, 1, figsize=(4, 1))
     # axes.axis('off')
-    plt.axhspan(min_normal, max_normal, color='green', alpha=0.3)
+    plt.axhspan(normal_min, normal_max, color='green', alpha=0.3)
+    axes.set_ylim([graph_y_min, graph_y_max])
     axes.plot(data_x, data_y)
 
     # Calculate major ticks for x-axis
@@ -45,15 +44,13 @@ def sparkline(data_x_str: list[str], data_y: list[float], min_normal, max_normal
     plt.savefig(img)
     img.seek(0)
     plt.close()
-
-    # fig.show()
     return '<img src="data:image/png;base64,{}"/>'.format(base64.b64encode(img.read()).decode())
 
 def sparklines(incoming: list[list[Observation]]) -> list[tuple[str, str]]:
     """
-    todo we need to pass in x and y
-    :param incoming:
-    :return:
+    Generate a list of sparklines.
+    :param incoming: a list of lists of Observations.
+    :return: a list of (image tag, stat name) tuples
     """
     outgoing = []
     for index in range(0, len(incoming)):
@@ -62,23 +59,44 @@ def sparklines(incoming: list[list[Observation]]) -> list[tuple[str, str]]:
             continue
 
         data_x = [x.date for x in one_ob_list]
-        data_y = [x.data[0].value for x in one_ob_list]
-        d1_min = 0.3 * (max(data_y) - min(data_y))  # TODO: We need to get this from referenceRange
-        d1_max = 0.8 * (max(data_y) - min(data_y))
-        img_info = sparkline(data_x, data_y, d1_min, d1_max)
+        data_y = [x.data[0].value for x in one_ob_list]  # TODO handle blood pressure and other multi-values stats.
+        BASELINE_ZERO = True
+        if BASELINE_ZERO:
+            normal_min = 0.3 * (max(data_y))  # TODO: We need to get this from referenceRange
+            normal_max = 0.8 * (max(data_y))
+            baseline = 0
+        else:
+            normal_min = 0.3 * (max(data_y) - min(data_y))  # TODO: We need to get this from referenceRange
+            normal_max = 0.8 * (max(data_y) - min(data_y))
+            baseline = normal_min
+        graph_y_min = 0
+        graph_y_max = max(data_y)
+        img_info = sparkline(data_x, data_y, graph_y_min, graph_y_max, normal_min, normal_max)
         outgoing.append((img_info, one_ob_list[0].name))
 
     return outgoing
 
 
 def html_page(f: TextIO, incoming):
+    """
+    Generate HTML page for the sparklines
+    :param f:
+    :param incoming:
+    :return:
+    """
     print("""<!DOCTYPE html><html><head><meta charset="utf-8" /><body>""", file=f)
     print("<h1>Sparklines</H1>", file=f)
     sparks = sparklines(incoming)
+    print("<table>", file=f)
     for imgtag, stat_name in sparks:
-        print(F"""<h1>{stat_name}</h1>""", file=f)
+        print("<tr>", file=f)
+        print("<td>", file=f)
+        print(F"""{stat_name}""", file=f)
+        print("</td><td>", file=f)
         print(imgtag, file=f)
-        print("<br>\n", file=f)
+        print("</td>\n", file=f)
+        print("</tr>", file=f)
+    print("</table>", file=f)
     print("""</body></html>""", file=f)
 
 if __name__ == "__main__":
@@ -86,6 +104,7 @@ if __name__ == "__main__":
     condition_path = base / "clinical-records"
 
     stats = ["Pulse", "Height", "Blood Pressure", "Weight", "Respirations", "SpO2", "Temperature"]
+    # stats = ["Height"]
     category_name = 'Vital Signs'
 
     stats_to_graph = []
