bb0c2b2e0823f67bf751f81667c5485129a2b145
HealthRecords <github.com3@worldware.com>
2025-08-23 21:38:57 -0700

    Added utilities that are common, but don't have internal dependencies, so everyone can include them.

--- config.py
+++ config.py
@@ -6,3 +6,15 @@ from pathlib import Path
 # TODO Right now, this is the root directory of expanded zip. Later, I want to just access
 # the zip file directly
 source_dir: Path = Path('/Users/tomhill/Downloads/apple_health_export')
+
+import re
+import unicodedata
+
+def sanitize_filename_manual(filename, max_length=128):
+    normalized = unicodedata.normalize('NFKD', filename)
+    cleaned = re.sub(r'[^a-zA-Z0-9.\-_ ]', ' ', normalized)
+    sanitized = cleaned.replace(' ', '_')
+    sanitized = sanitized[:max_length]
+    if not sanitized:
+        sanitized = 'unnamed_file'
+    return sanitized
