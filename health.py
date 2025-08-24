f25110b98d26633712d9ae7a03bcaeeb90b2de1a
HealthRecords <github.com3@worldware.com>
2025-08-23 21:38:55 -0700

    Added procedure printing Added ability to get categories for just one file type. Added "menu" mode, where you choose from menus.

--- health.py
+++ health.py
@@ -14,9 +14,14 @@ from collections import Counter
 # TODO There are over a dozen categories. I'm only printing a few, so far. I see, for example, Lab, Outpatient,
 #      Discharge,Imaging, medication (lowercase) and more. I may need some generalized way to print / sum /graph these.
 #      Maybe something like "jq".
+# TODO Finish interactive user interface. Observations is just getting started.
+# For interactive mode, I need to be consistent about print, plot, and active/inactive.
+# TODO like medicines, conditions should have an option to print inactive.
+# TODO command line args, categories and medicines are plural, but condition is singular. Should be consistent.
 # TODO print_condition and print_medicines should be generalized and combined.
 # TODO Do we want to have an option to process multiple or all stats in one run?
 # TODO Option to list all types of documents found.
+
 
 @dataclass
 class ValueQuantity:
@@ -162,6 +167,27 @@ def print_conditions(cd: Path, csv_format: bool, match: str) -> NoReturn:
             # Almost the same as csv, but the csv version escapes special characters, if there are any.
             print(condition)
 
+def print_procedures(cd: Path, csv_format: bool, match: str) -> NoReturn:
+    path = cd / match
+    conditions = []
+    for p in glob.glob(str(path)):
+        with open(p) as f:
+            condition = json.load(f)
+            conditions.append(
+                (condition['resourceType'],
+                 condition['performedDateTime'],
+                 condition['status'],
+                 condition['code']['text'],
+                 )
+            )
+    cs = sorted(conditions, key=lambda x: x[1])
+    for condition in cs:
+        if csv_format:
+            print_csv(condition)
+        else:
+            # Almost the same as csv, but the csv version escapes special characters, if there are any.
+            print(condition)
+
 def print_medicines(cd: Path, csv_format: bool, match: str, include_inactive: bool) -> NoReturn:
     path = cd / match
     conditions = []
@@ -244,9 +270,9 @@ def print_prefixes(dir_path: Path) -> NoReturn:
     for ext, count in extensions.items():
         print(F"{count:6} {ext}")
 
-def list_categories(dir_path: Path, only_first) -> Counter:
+def list_categories(dir_path: Path, only_first, *, one_prefix) -> (list[tuple], Counter, int):
     """
-    The schema of this data is not well-designed. I have seen category expressed three ways so far.
+    The schema of this data is not well-designed. I have seen category expressed FOUR ways so far.
 
     "category":
         "CAT_NAME"
@@ -268,14 +294,21 @@ def list_categories(dir_path: Path, only_first) -> Counter:
         }
     ]
 
+    :param one_prefix: There are different kinds of documents, that start with "Observation" or "MedicationRequest"
+    :                  For example, set this to "Observation" to only see categories from Observation files,
+                       or set to None for all files.
     :param dir_path: Path of the directory to scan.
     :param only_first:  Only take the first category in a file. This is so we can see if there are any files without
                         categories.
-    :return:
+    :return: c_sorted, counter, count
     """
     counter = Counter()
     count = 0
-    for p in dir_path.glob("*.json"):
+    wildcard = "*.json"
+    if one_prefix:
+        wildcard = one_prefix + wildcard
+
+    for p in dir_path.glob(wildcard):
         with open(p) as f:
             count += 1
             observation_data = json.load(f)
@@ -299,6 +332,17 @@ def list_categories(dir_path: Path, only_first) -> Counter:
                 raise ValueError(F"File {p} has no category", p)
 
     c_sorted = sorted(counter, key=lambda x: counter[x], reverse=True)
+    return c_sorted, counter, count
+
+def print_categories(dir_path: Path, only_first, *, one_prefix) -> NoReturn:
+    """
+
+    :param dir_path:
+    :param only_first:
+    :param one_prefix:
+    :return:
+    """
+    c_sorted, counter, count = list_categories(dir_path, only_first, one_prefix=one_prefix)
     print(F"Categories found in {count} files in {dir_path}")
 
     c2 = 0
@@ -306,7 +350,7 @@ def list_categories(dir_path: Path, only_first) -> Counter:
         print(F"{index:3}: {key:.<32}: {counter[key]:>6}")
         c2 += counter[key]
     print(F"                                       {"======":>6}")
-    print(F"{"":3}  Total files with categories.....: {c2:>6}")
+    print(F"{"":3}  Total categories found..........: {c2:>6}")
     print("Some files have more than one category. In particular, many files have both 'Lab' and 'Laboratory'")
 
     return counter
@@ -330,6 +374,8 @@ def parse_args():
                         help='Show the types of documents in the clinical-records directory')
     parser.add_argument('-g', '--generic', type=str,
                         help='Lets you specify a category and a code, like -g Vital-signs:Weight. See --categories')
+    parser.add_argument('-i', '--interactive', action=argparse.BooleanOptionalAction,
+                        help='In interactive mode,  you will be prompted for choices.')
     parser.add_argument('-l', '--list-vitals', action=argparse.BooleanOptionalAction,
                         help='List names of all vital signs that were found.')
     parser.add_argument('-m', '--medicines', action=argparse.BooleanOptionalAction,
@@ -347,8 +393,8 @@ def parse_args():
             'use the -l to get a list of stats found in your data.')
     args = parser.parse_args()
     active = [args.allergy, args.condition, args.document_types, args.list_vitals, args.medicines, args.medicines_all,
-              args.categories, args.stat, args.generic]
-    flags = ["-a", "-c", "-d", "-l", "-m", "--medicines-all", "--categories", "-s", "-g"]
+              args.categories, args.stat, args.generic, args.interactive]
+    flags = ["-a", "-c", "-d", "-l", "-m", "--medicines-all", "--categories", "-s", "-g", "-i"]
     return args, active, flags
 
 def plot(dates, values: list[float], values2: list[float], graph_subject, data_name_1, data_name_2) -> None:
@@ -399,20 +445,22 @@ def plot(dates, values: list[float], values2: list[float], graph_subject, data_n
 
 def do_vital(condition_path: Path, vital: str, after: str, print_data: bool, vplot: bool, csv_format: bool,
              *, category_name) -> NoReturn:
-    ws = extract_all_values(yield_observation_files(condition_path), vital, category_name=category_name)
-    if after:
-        ad = datetime.strptime(after, '%Y-%m-%d')
-        ws = [w for w in ws if ad < datetime.strptime(w.date, '%Y-%m-%dT%H:%M:%SZ')]
     if not print_data and not vplot:
         print("You need to select at least one of --plot or --print with --stat")
         return
+
+    ws = extract_all_values(yield_observation_files(condition_path), vital, category_name=category_name)
+
+    if after:
+        ad = datetime.strptime(after, '%Y-%m-%d')
+        ws = [w for w in ws if ad < datetime.strptime(w.date, '%Y-%m-%dT%H:%M:%SZ')]
+
     if not ws:
         print(F"No data was found for stat {vital} ")
         if after:
             print(F"In the range of values after {after}")
         print(F"You can use the -l argument to see what stats are in your data.")
         return
-
     if print_data:
         print_values(ws, csv_format)
 
@@ -438,6 +486,74 @@ def do_vital(condition_path: Path, vital: str, after: str, print_data: bool, vpl
         plot(dates, values_1, values_2, vital, data_name_1, data_name_2)
 
 
+def menu(choices: list[str]):
+    option = -1
+    while option < 1 or option > len(choices):
+        for index, choice in enumerate(choices):
+            print(f"[{index+1:3}] {choice}")
+        print(f"[{"q":>3}] {"quit"}")
+        print("Choose an option: ", end="")
+        c = input()
+        if c.strip() == "q":
+            return -1, "quit"
+        option = int(c)
+    return option - 1, choices[option - 1]
+
+def user_interface_observation(data_dir: Path, args):
+    """
+    Observations are anything measured. Test results, measurements of height or weight, etc.
+
+    :param data_dir:
+    :param args:
+    :return:
+    """
+    list_cat, dict_cat, file_count = list_categories(data_dir, False, one_prefix=None)
+    while (option := menu(list_cat))[0] != -1:
+        option_number, category = option
+        vitals = list_vitals(yield_observation_files(data_dir), category)
+        vital_list = [k for k in vitals.keys()]
+        while (choices := menu(vital_list))[0] != -1:
+            choice_number, choice_string = choices
+            do_vital(data_dir, choice_string, args.after, True, True, args.csv_format,
+                     category_name=category)
+        print("You want information about ", option[1])
+        # print("Would you like to print or plot this?")
+    return
+
+def user_interface(condition_path: Path, args) -> None:
+    """
+    display menus on the command line
+
+    :param args:
+    :param condition_path:
+    :return: No Return
+    """
+    print()
+    options = list(list_prefixes(condition_path).keys())
+    while (var := menu(options))[0] != len(options):
+        value = var[1]
+        match value:
+            case "quit":
+                return
+            case "Observation":
+                user_interface_observation(condition_path, args)
+            case "MedicationRequest":
+                include_inactive, v = menu(["Active Medicines", "All Medicines"])
+                include_inactive = bool(include_inactive)
+                print_medicines(condition_path, args.csv_format, "MedicationRequest*.json", include_inactive)
+            case "DocumentReference":
+                print("I don't know anything about DocumentReferences, yet.")
+            case "Condition":
+                print_conditions(condition_path, args.csv_format, "Condition*.json")
+            case "AllergyIntolerance":
+                print_conditions(condition_path, args.csv_format, "AllergyIntolerance*.json")
+            case "Procedure":
+                print_procedures(condition_path, args.csv_format, "Procedure*.json")
+            case _:
+                print("I don't know anything about " + value + " files, yet.")
+    return
+
+
 def go():
     args, active, flags = parse_args()
     base = Path("export/apple_health_export")
@@ -447,16 +563,20 @@ def go():
         print(F"Please select one of {flags} to get some output.")
         return
 
+    if args.interactive:
+        user_interface(condition_path, args)
+        return
+
     if args.condition:
         print_conditions(condition_path, args.csv_format, "Condition*.json")
 
     if args.allergy:
         print_conditions(condition_path, args.csv_format, "All*.json")
 
-    include_inactive = False
-    if args.medicines_all:
-        include_inactive = True
     if args.medicines or args.medicines_all:
+        include_inactive = False
+        if args.medicines_all:
+            include_inactive = True
         print_medicines(condition_path, args.csv_format, "MedicationRequest*.json", include_inactive)
 
     if args.stat:
@@ -466,7 +586,7 @@ def go():
         print_vitals(observation_files=yield_observation_files(condition_path), category="Vital Signs")
 
     if args.generic:
-        param = args.generic.split(":", 1)
+        param = args.generic.split("#", 1)
         assert isinstance(param, list)
         if len(param) == 1:
             print_vitals(observation_files=yield_observation_files(condition_path), category=param[0])
@@ -477,7 +597,7 @@ def go():
             print("Invalid format: use -g category:code     like '-g \"Vital Signs:Blood Pressure\"")
 
     if args.categories:
-        list_categories(condition_path, only_first=False)
+        print_categories(condition_path, only_first=False, one_prefix=None)
 
     if args.document_types:
         print_prefixes(condition_path)
--- test_health.py
+++ test_health.py
@@ -1,12 +1,12 @@
 from pathlib import Path
 from unittest import TestCase
-from health import extract_value, list_vitals, list_prefixes
+from health import extract_value, list_vitals, list_prefixes, list_categories
 
 
 class Test(TestCase):
     def test_extract_values(self):
         test_file = "test_data/Observation-test-bp.json"
-        observation = extract_value(test_file, "Blood Pressure")
+        observation = extract_value(test_file, "Blood Pressure", category_name="Vital Signs")
         self.assertEqual(observation.name, "Blood Pressure" )
         self.assertEqual(observation.date, '2024-02-15T21:00:03Z')
         self.assertEqual(observation.data[0].value, 130)
@@ -18,7 +18,7 @@ class Test(TestCase):
 
     def test_list_available(self):
         test_file = "test_data/Observation-test-bp.json"
-        vitals = list_vitals([test_file])
+        vitals = list_vitals([test_file], "Vital Signs")
         self.assertEqual(1, len(vitals))
         self.assertTrue("Blood Pressure" in vitals)
 
@@ -27,3 +27,14 @@ class Test(TestCase):
         self.assertEqual(2, len(vitals))
         self.assertEqual(2, vitals["Observation"])
         self.assertEqual(1, vitals["MedicationRequest"])
+
+    def test_categories(self):
+        category_list, category_counter, count = list_categories(Path("test_data"), False, one_prefix=None)
+        self.assertEqual(2, len(category_list))
+        self.assertEqual(1, category_counter["Community"])
+        self.assertEqual(2, category_counter["Vital Signs"])
+
+        category_list, category_counter, count = list_categories(Path("test_data"), False, one_prefix='Observation')
+        self.assertEqual(1, len(category_list))
+        self.assertFalse("Community" in category_counter)
+        self.assertEqual(2, category_counter["Vital Signs"])
