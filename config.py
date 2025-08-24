31854680b3fbe5242abb1c9613b8d477a0257245
HealthRecords <github.com3@worldware.com>
2025-08-23 21:38:57 -0700

    use a Path in config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 """
-Keep all config in once place, so it's not scattered thorughout the code.
+Keep all config in once place, so it's not scattered throughout the code.
 """
 from pathlib import Path
 
 # TODO Right now, this is the root directory of expanded zip. Later, I want to just access
 # the zip file directly
-source_dir: Path = '/Users/tomhill/Downloads/apple_health_export'
+source_dir: Path = Path('/Users/tomhill/Downloads/apple_health_export')
