f5708f13ed38954b306ebcd33d325f0a6b234648
HealthRecords <github.com3@worldware.com>
2025-08-23 21:38:56 -0700

    Working on handling more data format quirks.
    sort test output.

--- sparklines.py
+++ sparklines.py
@@ -1,5 +1,5 @@
 """
-This file generates a set of sparklines. Currrently hard coded.
+This file generates a set of sparklines. Currently hard coded.
 
 https://www.markhneedham.com/blog/2017/09/23/python-3-create-sparklines-using-matplotlib/
 
@@ -14,6 +14,8 @@ TODO: Need to match the date range across all sparklines, if I'm going to line t
 TODO: I need normal range for every test we want to plot (I guess it's not required, just nice to have).
       It looks like what I want is in Observation-*.json files,
       as referenceRange. It's in 2707 out of 4541 files.
+
+TODO: This file has a test name of "---": Observation-7881B1CD-55FD-42BD-8FFB-CE98D13C88CD.json, fix it.
 """
 import base64
 from datetime import datetime
@@ -24,10 +26,12 @@ from typing import TextIO
 from matplotlib import pyplot as plt
 import matplotlib.dates as mdates
 
-from health import extract_all_values, yield_observation_files, Observation, StatInfo
+from health import extract_all_values, yield_observation_files, Observation, StatInfo, print_vitals, list_vitals
 
 
 def sparkline(data_x_str: list[str], data_y: list[float], graph_y_min, graph_y_max, normal_min, normal_max):
+    # We assumed that normal would be horizontal lines. We have found some tests that have a referenceRange
+    # for some values, and not for others. I think there is a way to shade between curves. Try that.
     data_x = [datetime.strptime(date, '%Y-%m-%dT%H:%M:%SZ') for date in data_x_str]
 
     fig, axes = plt.subplots(1, 1, figsize=(4, 1))
@@ -38,7 +42,11 @@ def sparkline(data_x_str: list[str], data_y: list[float], graph_y_min, graph_y_m
     else:
         normal_max = graph_y_max
     axes.set_ylim([graph_y_min, max(graph_y_max, normal_max)])
-    axes.plot(data_x, data_y)
+    if len(data_x) == 1:
+        # Single points are invisible, so make it more obvious.
+        axes.plot(data_x, data_y, 'o', markeredgecolor = 'r')
+    else:
+        axes.plot(data_x, data_y,)
 
     # Calculate major ticks for x-axis
     years = mdates.YearLocator()
@@ -66,10 +74,21 @@ def sparklines(incoming: list[list[Observation]]) -> list[tuple[str, str]]:
         data_x = [x.date for x in one_ob_list]
         data_y = [x.data[0].value for x in one_ob_list]  # TODO handle blood pressure and other multi-values stats.
         if one_ob_list[0].range is not None:
-            range_ = {x.range.low.value for x in one_ob_list}
-            assert len(range_) == 1
-            normal_min = one_ob_list[0].range.low.value
-            normal_max = one_ob_list[0].range.high.value
+            check_for_messy_data = False
+            nn = [x.range for x in one_ob_list]
+            if None in nn:
+                print("We have a set of observations with different range limit values for the same teste. "
+                      "This is in the data, not a bug in code. Need to figure out what to do with it.", one_ob_list[0].filename)
+                if check_for_messy_data:  # TODO
+                    range_ = {x.range.low.value for x in one_ob_list}
+                    assert len(range_) == 1
+            if not hasattr(one_ob_list[0].range, "low") or not hasattr(one_ob_list[0].range.low, "value"):
+                print("Missing low or value in the data:", one_ob_list[0].filename)
+                normal_min = None
+                normal_max = None
+            else:
+                normal_min = one_ob_list[0].range.low.value
+                normal_max = one_ob_list[0].range.high.value
         else:
             normal_min = None
             normal_max = None
@@ -81,7 +100,7 @@ def sparklines(incoming: list[list[Observation]]) -> list[tuple[str, str]]:
         graph_y_min = 0
         graph_y_max = max(data_y)
         img_info = sparkline(data_x, data_y, graph_y_min, graph_y_max, normal_min, normal_max)
-        outgoing.append((img_info, one_ob_list[0].name))
+        outgoing.append((img_info, one_ob_list[0].name, len(one_ob_list)))
 
     return outgoing
 
@@ -97,10 +116,10 @@ def html_page(f: TextIO, incoming):
     print("<h1>Sparklines</H1>", file=f)
     sparks = sparklines(incoming)
     print("<table>", file=f)
-    for imgtag, stat_name in sparks:
+    for imgtag, stat_name, count in sparks:
         print("<tr>", file=f)
         print("<td>", file=f)
-        print(F"""{stat_name}""", file=f)
+        print(F"""{stat_name}({count})""", file=f)
         print("</td><td>", file=f)
         print(imgtag, file=f)
         print("</td>\n", file=f)
@@ -123,15 +142,31 @@ if __name__ == "__main__":
         StatInfo("Vital Signs", "Blood Pressure"),
         StatInfo("Vital Signs", "Weight"),
         StatInfo("Vital Signs", "Respirations"),
-        StatInfo("Vital Signs", "SpO2"),
         StatInfo("Vital Signs", "Temperature")
     ]
+    # TODO: Should we sort by frequency of test? Recency of test? Name of test?
+    stats = sorted(stats, key=lambda x: (x.name, x.category_name))
     # category_name = 'Vital Signs'
-
+    # stats = [StatInfo("Lab", "PSA"), StatInfo("Lab", "PROSTATE SPECIFIC ANTIGEN (PSA)")]
+    # stats = [StatInfo("Lab", "PSA"), StatInfo("Lab", "PROSTATE SPECIFIC ANTIGEN (PSA)")]
     stats_to_graph = []
     for vital in stats:
         ws = extract_all_values(yield_observation_files(condition_path), stat_info=vital)
         stats_to_graph.append(ws)
 
     with open("sparklines.html", "w") as fff:
+        html_page(fff, stats_to_graph)
+
+    l = list_vitals(yield_observation_files(condition_path), "Lab")
+    print(l)
+
+    stats = [StatInfo("Lab", x) for x in l]
+    stats = sorted(stats, key=lambda x: (x.name, x.category_name))
+
+    stats_to_graph = []
+    for vital in stats:
+        ws = extract_all_values(yield_observation_files(condition_path), stat_info=vital)
+        stats_to_graph.append(ws)
+
+    with open("sparklines_all.html", "w") as fff:
         html_page(fff, stats_to_graph)
