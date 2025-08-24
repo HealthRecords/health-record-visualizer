8372533bd86778d066187fc4943ca0cc99f66f93
HealthRecords <github.com3@worldware.com>
2025-08-23 21:38:55 -0700

    Collect name of multiple valued fields, like blood pressure

--- test_health.py
+++ test_health.py
@@ -11,6 +11,8 @@ class Test(TestCase):
         self.assertEqual(w[1], '2024-02-15T21:00:03Z')
         self.assertEqual(w[2][0][0], 130)
         self.assertEqual(w[2][0][1], 'mm[Hg]')
+        self.assertEqual(w[2][0][2], 'Systolic blood pressure')
         self.assertEqual(w[2][1][0], 88)
         self.assertEqual(w[2][1][1], 'mm[Hg]')
+        self.assertEqual(w[2][1][2], 'Diastolic blood pressure')
 
