206ae1b5d90cc88d05cb6cc17ec1d1ce879e2c8f
HealthRecords <github.com3@worldware.com>
2025-08-23 21:38:54 -0700

    Moving to generators. Added listing of all available vital signs.

--- health.py
+++ health.py
@@ -1,12 +1,12 @@
 import glob
 import json
 from pathlib import Path
-from typing import NoReturn
+from typing import NoReturn, Generator, Iterable
 import argparse
 
 # TODO Print mix and max values?
 
-def extract_value(file: str, sign_name: str) -> tuple|None:
+def extract_value(file: str, sign_name: str) -> tuple | None:
     with open(file) as f:
         condition = json.load(f)
         category_info = condition['category']
@@ -33,10 +33,30 @@ def extract_value(file: str, sign_name: str) -> tuple|None:
                         return t, d, sub_values
     return None
 
-def extract_all_values(cd: Path, sign_name: str) -> list[tuple]:
+def yield_observations(cd: Path) -> Iterable[str]:
     path = cd / "Observation*.json"
-    values = []
     for p in glob.glob(str(path)):
+        yield p
+
+def filter_category(observation_files: Iterable[str], code_text: str) -> Iterable[dict]:
+    """
+    Filters observations, only passing on those with a code['text'] = code_text
+    :param observation_files: Source for file names
+    :param code_text: The name of the category to keep, like 'Vital Signs'
+    :return:
+    """
+    for file in observation_files:
+        with open(file) as f:
+            observation = json.load(f)
+            category_info = observation['category']
+            assert isinstance(category_info, list)
+            for ci in category_info:
+                if ci['text'] == "Vital Signs":
+                    yield observation
+
+def extract_all_values(observations: Iterable[str], sign_name: str) -> list[tuple]:
+    values = []
+    for p in observations:
         value = extract_value(p, sign_name)
         if value is not None:
             values.append(value)
@@ -77,20 +97,32 @@ def print_values(ws: list[tuple]) -> NoReturn:
         print_value(w)
 
 
+def list_vitals(observation_files: Iterable[str]) -> NoReturn:
+    vitals = set()
+    signs_found = filter_category(observation_files, "Vital Signs")
+    for observation in signs_found:
+        code_name = observation['code']['text']
+        vitals.add(code_name)
+    print("Vital Statistics found in records.")
+    for stat in vitals:
+        print("\t", stat)
+
 def parse_args():
     parser = argparse.ArgumentParser(description='Explore Kaiser Health Data')
 
     # Add verbose argument
     parser.add_argument('-v', '--vital', type=str,
-        help='Print a vital statistic, like weight. Name has to match EXACTLY, "Weight" is not "weight".\nSome examples:\n'
-                        +'SpO2, Weight, "Blood Pressure" (quotes are required, if the name has spaces in it).')
+        help='Print a vital statistic, like weight. Name has to match EXACTLY, ' +
+            'Weight" is not "weight".\nSome examples:\n' +
+            'SpO2, Weight, "Blood Pressure" (quotes are required, if the name has spaces in it).')
     parser.add_argument('-c', '--condition', action='store_true', help='Print all active conditions.')
+    parser.add_argument('-l', '--list-vitals', action='store_true', help='Print all active conditions.')
     # Parse the command-line arguments
     args = parser.parse_args()
-    return args.vital, args.condition
+    return args.vital, args.condition, args.list_vitals
 
 def go():
-    vital, condition = parse_args()
+    vital, condition, lv = parse_args()
     base = Path("export/apple_health_export")
     condition_path = base / "clinical-records"
 
@@ -98,9 +130,11 @@ def go():
         print_conditions(condition_path)
 
     if vital:
-        ws = extract_all_values(condition_path, vital)
+        ws = extract_all_values(yield_observations(condition_path), vital)
         print_values(ws)
 
+    if lv:
+        list_vitals(observation_files=yield_observations(condition_path))
 
 if __name__ == "__main__":
     go()
