3dfc7693df646760e8bbf1c1ce153b30f2761d4c
HealthRecords <github.com3@worldware.com>
2025-08-23 21:38:56 -0700

    Add test for group by days.

--- test_sparkbase.py
+++ test_sparkbase.py
@@ -6,13 +6,15 @@ from sparkbase import group_by_days
 
 class Test(TestCase):
     def test_group_by_days(self):
-        one: Observation = Observation(name="One", date="2021-01-01T10:01:02")
-        two: Observation = Observation(name="Two", date="2021-01-01T10:03:04")
-        thr: Observation = Observation(name="Thr", date="2021-01-01T11:05:06")
-        fou: Observation = Observation(name="Fou", date="2021-01-02T11:05:06")
-        fiv: Observation = Observation(name="Fiv", date="2021-01-02T13:07:08")
-        six: Observation = Observation(name="Six", date="2021-01-03T14:09:10")
+        one: Observation = Observation(name="One", date="2021-01-01T10:01:02", source_name="X")
+        two: Observation = Observation(name="Two", date="2021-01-01T10:03:04", source_name="X")
+        thr: Observation = Observation(name="Thr", date="2021-01-01T11:05:06", source_name="Y")
+        fou: Observation = Observation(name="Fou", date="2021-01-02T11:05:06", source_name="X")
+        fiv: Observation = Observation(name="Fiv", date="2021-01-02T13:07:08", source_name="X")
+        six: Observation = Observation(name="Six", date="2021-01-03T14:09:10", source_name="Y")
         data = [[one, two, thr, fou, fiv, six]]
         out = group_by_days(data)
         self.assertEqual(out, [[one, two, thr],[fou,fiv],[six]])
+        out = group_by_days(data, "X")
+        self.assertEqual(out, [[one, two],[fou,fiv]])
 
