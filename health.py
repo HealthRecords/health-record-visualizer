e2ee81d0ab3cceb092026e63b46eecbc00edd9ce
HealthRecords <github.com3@worldware.com>
2025-08-23 21:38:54 -0700

    generalized wieght printing to print other values like SpO2

--- health.py
+++ health.py
@@ -4,9 +4,11 @@ from pathlib import Path
 from typing import NoReturn
 import argparse
 
-def extract_weights(cd: Path) -> list[tuple]:
+# TODO Print mix and max values?
+
+def extract_values(cd: Path, sign_name: str) -> list[tuple]:
     path = cd / "Observation*.json"
-    weights = []
+    values = []
     for p in glob.glob(str(path)):
         with open(p) as f:
             condition = json.load(f)
@@ -18,14 +20,14 @@ def extract_weights(cd: Path) -> list[tuple]:
             assert isinstance(category_info, list)
             for ci in category_info:
                 if ci['text'] == "Vital Signs":
-                    if condition['code']['text'] == "Weight":
-                        t = "Weight"
+                    if condition['code']['text'] == sign_name:
+                        t = sign_name
                         d = condition['effectiveDateTime']
                         w = condition["valueQuantity"]["value"]
                         u = condition["valueQuantity"]["unit"]
-                        weights.append((t, d, w, u))
-    weights = sorted(weights, key=lambda x: x[1])
-    return weights
+                        values.append((t, d, w, u))
+    values = sorted(values, key=lambda x: x[1])
+    return values
 
 def print_conditions(cd: Path):
     path = cd / "Condition*.json"
@@ -45,17 +47,24 @@ def print_conditions(cd: Path):
     for c in cs:
         print(c)
 
-def print_weights(ws: list[tuple]) -> NoReturn:
+def print_value(w: tuple):
+    print(F"{w[0]:10}: {w[1]} - {w[2]:6.1f} {w[3]}", end="")
+    if w[3] == "kg":
+        print(f": {w[2] * 2.2:6.1f}")
+    print()
+
+def print_values(ws: list[tuple]) -> NoReturn:
     for w in ws:
-        assert w[3] == "kg"
-        print(F"{w[0]:10}: {w[1]} - {w[2]:6.1f} {w[3]}: {w[2]*2.2:6.1f}")
+        print_value(w)
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Explore Kaiser Health Data')
 
     # Add verbose argument
-    parser.add_argument('-v', '--vital', action='store_true', help='Print a vital statistic, like weight.')
+    parser.add_argument('-v', '--vital', type=str,
+        help='Print a vital statistic, like weight. Name has to match EXACTLY, "Weight" is not "weight".\nSome examples:\n'
+                        +'SpO2, Weight, "Blood Pressure" (quotes are required, if the name has spaces in it).')
     parser.add_argument('-c', '--condition', action='store_true', help='Print all active conditions.')
     # Parse the command-line arguments
     args = parser.parse_args()
@@ -70,8 +79,8 @@ def go():
         print_conditions(condition_path)
 
     if vital:
-        ws = extract_weights(condition_path)
-        print_weights(ws)
+        ws = extract_values(condition_path, vital)
+        print_values(ws)
 
 
 if __name__ == "__main__":
