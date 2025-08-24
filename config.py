8f9b92d1eede471a6eaa8bbd1ed2a18e2297c60a
HealthRecords <github.com3@worldware.com>
2025-08-23 21:38:57 -0700

    Define the source dir in one place. Not all places use this yet.

---
+++ config.py
@@ -0,0 +1,8 @@
+"""
+Keep all config in once place, so it's not scattered thorughout the code.
+"""
+from pathlib import Path
+
+# TODO Right now, this is the root directory of expanded zip. Later, I want to just access
+# the zip file directly
+source_dir: Path = '/Users/tomhill/Downloads/apple_health_export'
