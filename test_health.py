fe3222bfd7a970fd73e23e887da5af0c7e900911
HealthRecords <github.com3@worldware.com>
2025-08-23 21:38:55 -0700

    Move test data to test_data directory.

--- test_health.py
+++ test_health.py
@@ -5,7 +5,7 @@ from health import extract_value, list_vitals, list_prefixes
 
 class Test(TestCase):
     def test_extract_values(self):
-        test_file = "Observation-test-bp.json"
+        test_file = "test_data/Observation-test-bp.json"
         observation = extract_value(test_file, "Blood Pressure")
         self.assertEqual(observation.name, "Blood Pressure" )
         self.assertEqual(observation.date, '2024-02-15T21:00:03Z')
@@ -17,12 +17,13 @@ class Test(TestCase):
         self.assertEqual(observation.data[1].name, 'Diastolic blood pressure')
 
     def test_list_available(self):
-        test_file = "Observation-test-bp.json"
+        test_file = "test_data/Observation-test-bp.json"
         vitals = list_vitals([test_file])
         self.assertEqual(1, len(vitals))
         self.assertTrue("Blood Pressure" in vitals)
 
     def test_list_prefixes(self):
-        vitals = list_prefixes(Path("."))
-        self.assertEqual(1, len(vitals))
+        vitals = list_prefixes(Path("test_data"))
+        self.assertEqual(2, len(vitals))
         self.assertEqual(2, vitals["Observation"])
+        self.assertEqual(1, vitals["MedicationRequest"])
